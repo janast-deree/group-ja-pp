939f29dc476221bca455c03d53d0b23970055299
James Anastasopoulos <james.anastasopoulos@marinetraffic.com>
2021-10-13 18:27:44 +0300

    fixed icon issue

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     path_d_desc = "M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z";
     path_d_asc = "M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293V3.5zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1h-1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1h-3zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1h-5zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5z";
 
-    $('#sortIcon').find('path').attr('d', path_d_asc);
+    $('path#sortIcon').attr('d', path_d_asc);
 
     //Add row fuction
     $('#add').on('click', function () {
@@ -60,12 +60,12 @@ $(document).ready(function () {
             $sortDir = "desc";
             $(this).removeClass("asc");
             $(this).addClass("desc");
-            $(this).find('path').attr("d", path_d_desc);
+            $(this).find('path#sortIcon').attr("d", path_d_desc);
         }
         else {
             $(this).removeClass("desc");
             $(this).addClass("asc");
-            $(this).find("path").attr("d", path_d_asc);
+            $(this).find("path#sortIcon").attr("d", path_d_asc);
         }
         sort($sortBy, $sortDir);
     })
