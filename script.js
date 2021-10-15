2a5368cc2d223f2050674fa7ce2399fa0af44a17
nihilp <p.panagiotopoulos@acg.edu>
2021-10-15 15:23:38 +0300

    Fixed numeric sorting, worked on validation

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// if ($title == '' ){
+//     errorMessage += " Enter your Name<br/>";
+//     errorCounter++;
+// }
+
+// if ($rating == '') {
+//     errorMessage += " Enter your age<br/>";
+//     errorCounter++;
+// }
+// else if (!$.isNumeric($rating)) {
+//     errorMessage += " Invalid age-should be numeric<br/>";
+//     errorCounter++;
+// }
+
 
 $(document).ready(function () {
+
     //d attribute values for the sorting svg
     path_d_desc = "M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z";
     path_d_asc = "M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293V3.5zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1h-1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1h-3zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1h-5zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5z";
@@ -71,7 +86,7 @@ $(document).ready(function () {
     })
 
 
-    //TODO: fix sorting by rating (currently due to str comparison it orders as: 1,10,2,3,... instead of 1,2,3,...,10)
+    //Sort by rating
     function sort(sortBy, sortDir) {
 
         var rows = $('tbody').children();
@@ -80,14 +95,16 @@ $(document).ready(function () {
             var first_val = $(first).find(sortBy).text().toUpperCase();
             var second_val = $(second).find(sortBy).text().toUpperCase();
             if (sortDir == "asc") {
-                return (first_val < second_val) ? -1 : (first_val > second_val) ? 1 : 0;
+                return first_val.localeCompare(second_val, false, { numeric: true });
             }
             else if (sortDir == "desc") {
-                return (first_val < second_val) ? 1 : (first_val > second_val) ? -1 : 0;
+                return second_val.localeCompare(first_val, false, { numeric: true });
             }
         })
 
         $('tbody').append(rows);
     }
 });
+
+
 
